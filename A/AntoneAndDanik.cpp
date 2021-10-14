262b8c54350965d7ef597a30e65baca197f2e36f
Aadil Khan <aadilkhan3109@gmail.com>
2021-10-14 16:54:22 -0700

    init_commit

--- A/AntoneAndDanik.cpp
+++ A/AntoneAndDanik.cpp
@@ -3,15 +3,20 @@
 using namespace std;
 
 int main(){
-	int n,a=0,d=0;
-	cin>>n;
-	while(n--){
-		char t;
-		cin>>t;
-		if(t=='A') a++;
-		else d++;
+	int n, a = 0, d = 0;
+	char t;
+	cin >> n;
+	while (n--){
+		cin >> t;
+		if (t == 'A')
+			a++;
+		else
+			d++;
 	}
-	if(a>d) cout<<"Anton"<<endl;
-	else if(d>a) cout<<"Danik"<<endl;
-	else cout<<"Friendship"<<endl;
+	if (a > d)
+		cout << "Anton" << endl;
+	else if (d > a)
+		cout << "Danik" << endl;
+	else
+		cout << "Friendship" << endl;
 }
